eccd1dbf8bff383d45d47e0ba9ee26fded803cd0
iRumba <reloader2003@gmail.com>
2025-12-14 18:54:47 +0700

    init

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,1 +1,14 @@
-print("Hello world")
+from flask import Flask, render_template
+
+# Создаем экземпляр приложения Flask
+app = Flask(__name__)
+
+# Создаем маршрут для главной страницы
+@app.route('/')
+def hello():
+    # Рендерим шаблон hello.html
+    return render_template('hello.html')
+
+# Запускаем приложение
+if __name__ == '__lab1__':
+    app.run(debug=True)
